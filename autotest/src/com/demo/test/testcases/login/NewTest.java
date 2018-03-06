0ca05ab7c103fa2ed02b23928e763c08ac5373a3
zsxmjw <zsxmjw@126.com>
2018-03-06 15:49:33 +0800

    第四版

--- autotest/src/com/demo/test/testcases/login/NewTest.java
+++ autotest/src/com/demo/test/testcases/login/NewTest.java
@@ -9,8 +9,8 @@ import com.demo.test.utils.SelectBrowser;
 public class NewTest {
   @Test
   public void f() {
-	  System.setProperty("webdriver.chrome.driver", "res/driver/chrome/win/chromedriver.exe");
-	  WebDriver driver = new ChromeDriver();
-	  driver.get("https://www.baidu.com/");
+//	  System.setProperty("webdriver.chrome.driver", "res/driver/chrome/win/chromedriver.exe");
+//	  WebDriver driver = new ChromeDriver();
+//	  driver.get("https://www.baidu.com/");
   }
 }
